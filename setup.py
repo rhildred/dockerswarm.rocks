56bb243323102dc7871907c6be8bb2f62ba5752e
Rich Hildred <rhildred@gmail.com>
2019-08-26 18:31:21 +0000

    adding complete first stage setup

--- setup.py
+++ setup.py
@@ -2,4 +2,26 @@
 
 # this is a script that can be run on a series of amazon linux 2 machines to set up a docker swarm
 
-print("This will be setting up")
+import subprocess
+
+#subprocess.call(["sudo", "yum", "update"])
+#subprocess.call(["sudo", "yum", "install", "git", "amazon-efs-utils"])
+#sHostName = raw_input("enter desired hostname: ")
+#subprocess.call(["sudo", "hostnamectl", "set-hostname",  sHostName])
+
+# setup some swap
+#subprocess.call("sudo dd if=/dev/zero of=/swapfile bs=1M count=8192", shell=True)
+#subprocess.call(["sudo", "chmod","600", "/swapfile"])
+#subprocess.call(["sudo", "mkswap", "/swapfile"])
+#subprocess.call(["sudo", "swapon", "/swapfile"])
+#subprocess.call("sudo sh -c \"echo '/swapfile swap swap defaults 0 0'>>/etc/fstab\"", shell=True)
+
+# mount the data volume
+#subprocess.call("sudo mkdir /g", shell=True)
+#subprocess.call("sudo sh -c \"echo 'fs-2ec58e85:/ /g efs defaults,_netdev 0 0'>>/etc/fstab\"", shell=True)
+#subprocess.call("sudo mount -a", shell=True)
+
+# do the initial docker setup
+subprocess.call("sudo amazon-linux-extras install docker", shell=True)
+subprocess.call("sudo service docker start", shell=True)
+subprocess.call("sudo usermod -a -G docker ec2-user", shell=True)
