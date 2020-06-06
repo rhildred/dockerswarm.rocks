8d16644d33fca55892eb8e90080fed9efd690457
Rich Hildred <rhildred@gmail.com>
2020-06-06 16:57:01 +0000

    simpler setup yet

---
+++ setup3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set hostname
+echo -n "enter desired fully qualified hostname: "
+read HOSTNAME
+sudo hostnamectl set-hostname $HOSTNAME
+
+# setup some swap
+sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo sh -c "echo '/swapfile swap swap defaults 0 0'>>/etc/fstab"
+
+# create ubuntu user with sudo privileges
+
+echo 'You will still need to set up some authorized_keys'
+echo 'Also please set `PermitRootLogin no`'
+echo '`PasswordAuthentication no` and `ChallengeResponseAuthentication no`'
